127b268dab869e178ce7f0e45cc58373f11335e6
pcabanel <pascal.cabanel@gmail.com>
2021-05-25 10:26:58 +0200

    Example of reading a UBL XML file

---
+++ UBL/Xsd2Code.XmlSchema.UBLSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xsd2Code.XmlSchema.UBL;
+
+namespace ConsoleAppUBL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello UBL World!");
+
+            Console.WriteLine("Load XML UBL file");
+            var invoice = InvoiceType.LoadFromFile(@"..\..\..\xml\UBL-Invoice-2.0-Enveloped.xml");
+            invoice.SaveToFile(@"..\..\..\xml\UBL-Invoice-2.0-Enveloped2.xml");
+        }
+    }
+}
