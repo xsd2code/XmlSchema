71f2fe1f367e74737bc1c0e7f40d87580e75dd5d
Emirkan CAKIR <emirkan.cakir@epsi.fr>
2021-07-08 15:59:10 +0200

    FpML added

---
+++ Library/Xsd2Code.XmlSchema.FpMLSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Xsd2Code.XMLSchema.FpML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Fpml = requestAllocationHeader.LoadFromFile(@"\..\..\..\FpML_Example.xml");
+            Fpml.SaveToFile(@"..\..\..\xml\FpML-Enveloped2.xml");
+
+        }
+    }
+}
